aeab0fd35ba6f65ac286c9e4b80c9206e85871c7
314159265358x <314159265358x@gmail.com>
2023-04-29 19:04:20 +0800

    update 1.19.81.01 fix log

--- mods/achieveguard/src/main.cpp
+++ mods/achieveguard/src/main.cpp
@@ -15,13 +15,13 @@ void onLoad(ModContext*){
 haddri hook_impl_##i((void*)(j),(void*)(k));\
 hook_impl_##i.enable();
 
-hook_impl(0,0x062AF720,achieveguard_patch_2);
-hook_impl(1,0x062AF6D0,achieveguard_patch_1);
-hook_impl(2,0x062AF600,achieveguard_patch_2);
-hook_impl(3,0x062AF610,achieveguard_patch_2);
-hook_impl(4,0x05E3CDD0,achieveguard_patch_3);
-hook_impl(5,0x05E20F00,achieveguard_patch_3);
-hook_impl(6,0x05E3C880,achieveguard_patch_4);
+hook_impl(0,0x06567B20,achieveguard_patch_2);
+hook_impl(1,0x06563820,achieveguard_patch_1);
+hook_impl(2,0x06567A70,achieveguard_patch_2);
+hook_impl(3,0x06563760,achieveguard_patch_2);
+hook_impl(4,0x0611CFB0,achieveguard_patch_3);
+hook_impl(5,0x06104980,achieveguard_patch_3);
+hook_impl(6,0x0611C9C0,achieveguard_patch_4);
 
 #undef hook_impl
 };
--- mods/achieveguard/src/patch.cpp
+++ mods/achieveguard/src/patch.cpp
@@ -5,28 +5,28 @@
 
 void achieveguard_patch_1(void* p1){
 char* v1=reinterpret_cast<char*>(p1);
-do_log_debug("h1 %hx",v1[1116]);
+do_log_debug("h1 %hx",v1[1172]);
 v1[1172]=0;
 };
 
 bool achieveguard_patch_2(void* p1){
 char* v1=reinterpret_cast<char*>(p1);
-do_log_debug("h2 %hx",v1[1116]);
+do_log_debug("h2 %hx",v1[1172]);
 v1[1172]=0;
 return false;
 };
 
 bool achieveguard_patch_3(void* p1){
 char* v1=reinterpret_cast<char*>(p1);
-do_log_debug("h3 %hx",v1[72]);
-v1[72]=0;
+do_log_debug("h3 %hx",v1[80]);
+v1[80]=0;
 return false;
 };
 
 void* achieveguard_patch_4(void* p1,bool){
 char* v1=reinterpret_cast<char*>(p1);
-do_log_debug("h5 %hx",v1[72]);
-v1[72]=0;
+do_log_debug("h5 %hx",v1[80]);
+v1[80]=0;
 return p1;
 };
 
